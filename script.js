fe98ce5611c25eb00ca5021f9b2956a274f196d6
ChristianStander <stander.christian@gmail.com>
2023-04-20 17:08:41 +0200

    finsihed the hex app

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
-const count = Math.random();
-count = Math.random().toString();
+const count = document.querySelector('.count')
+const generate = document.querySelector('.generate')
+
+generate.addEventListener('click', () => {
+    
+    const hex = Math.random().toString(16).substring(2, 8)
+    count.innerHTML = "#" + hex
+    document.body.style.backgroundColor = "#" + hex
+})
+
+
